61c7224e0fbf42f701b6175655d712172105ef5b
Luann Mariano <luann.mariano2003@gmail.com>
2025-06-06 22:00:45 -0300

    Update solicitacoesService.js

--- src/services/solicitacoesService.js
+++ src/services/solicitacoesService.js
@@ -1,6 +1,6 @@
 export async function enviarSolicitacao(solicitacao) {
     try {
-        const response = await fetch('http://10.0.2.2:8080/solicitacoes', { // substitua pelo seu IP real
+        const response = await fetch('http://10.0.2.2:8080/solicitacoes', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
